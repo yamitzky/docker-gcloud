f7c8455aa9cfd13b59539b01ea83c785bd94c813
Yamitzky <negiga@gmail.com>
2019-04-01 17:38:47 +0900

    Update to handle AWS Fargate token

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM  google/cloud-sdk:alpine
 
+RUN apk add --no-cache jq
+
 COPY docker-entrypoint.sh /usr/local/bin/
 
 ENTRYPOINT ["docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,12 @@ if [[ ! -z "${GOOGLE_CLOUD_CREDENTIAL}" ]]; then
   GOOGLE_CLOUD_KEY_FILE=/tmp/key.json
 fi
 
+if [[ ! -z "${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}" ]]; then
+  curl -sL 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI > /tmp/aws.json
+  export AWS_ACCESS_KEY_ID=$(cat /tmp/aws.json | jq -r ".AccessKeyId")
+  export AWS_SECRET_ACCESS_KEY=$(cat /tmp/aws.json | jq -r ".SecretAccessKey")
+  export AWS_SECURITY_TOKEN=$(cat /tmp/aws.json | jq -r ".Token")
+fi
+
 gcloud auth activate-service-account --key-file $GOOGLE_CLOUD_KEY_FILE 
 $@
